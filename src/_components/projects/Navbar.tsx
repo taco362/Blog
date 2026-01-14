ec225fc2c379cbeebf8cb1f9800cb0f073fdf3a2
taco362 <gomsae1234@gmail.com>
2026-01-14 21:23:46 +0900

    Modify navbar width

--- src/_components/projects/Navbar.tsx
+++ src/_components/projects/Navbar.tsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="py-6 fixed top-0 bg-black min-w-7xl">
-      <div className="flex items-center justify-between px-6">
+    <nav className="py-6 fixed top-0 bg-black w-full">
+      <div className="flex items-center justify-between px-6 max-w-7xl mx-auto">
         <div className="flex gap-8 max-w-5xl text-lg font-thin">
             {navLinks.map((link) => (
         <Link key={link.id} href={link.href} className="trasition-all duration-200 hover:-translate-y-1">
